ad0977c5fe3aa902a2cb90f8783320baa2cf6a32
ajo||SC <stormcraftmcpe@gmail.com>
2017-07-18 18:20:52 +0700

    Create Main.php

---
+++ src/NoDrop/Main.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace NoDrop;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\utils\TextFormat as C;
+
+class Main extends PluginBase implements Listener

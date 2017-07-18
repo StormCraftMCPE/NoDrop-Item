86f2b571afd3cdefe8fcc0d1eb306d3298b179f0
ajo||SC <stormcraftmcpe@gmail.com>
2017-07-18 18:27:51 +0700

    Update Main.php

--- src/NoDrop/Main.php
+++ src/NoDrop/Main.php
@@ -8,4 +8,17 @@ use pocketmine\Player;
 use pocketmine\event\player\PlayerDropItemEvent;
 use pocketmine\utils\TextFormat as C;
 
-class Main extends PluginBase implements Listener
+class Main extends PluginBase implements Listener{
+  
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getLogger()->info(C::GREEN."NoDrop Enable!");
+  }
+  
+  public function onDisable(){
+    $this->getLogger()->info(C::RED."NoDrop Disable!");}
+  
+  public function onDropItem(PlayerDropItemEvent $event){
+   if($event->getPlayer()->getGamemode() === 1) $event->setCancelled();
+  }
+  
